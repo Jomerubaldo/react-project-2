2e8ba00211219e4468eddbbc8d21f7ce76616a9e
Your Name <jomerubaldo@gmail.com>
2025-07-13 00:37:11 +0800

    initial commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,19 +1,16 @@
+import { useState } from 'react';
+
 function App() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(event.target.elements.fname.value);
-    console.log(event.target.elements.lname.value);
-  };
+  const [name, setName] = useState('Jomerubaldo');
+  const [isChecked, setIsChecked] = useState(true);
+  const [fruits, setFruits] = useState(['Apple', 'Banana', 'Orange']);
+  const [object, setObject] = useState({ name: 'Jomer', age: 21 });
 
   return (
     <>
-      <form className="p-10 gap-5" onSubmit={handleSubmit}>
-        <input type="text" name="fname" placeholder="enter firstname" />
-        <input type="text" name="lname" placeholder="enter lastname" />
-        <button type="submit" className="bg-green-400 px-4 py-2">
-          Click
-        </button>
-      </form>
+      {fruits.map((fruits, index) => (
+        <li key={index}>{fruits}</li>
+      ))}
     </>
   );
 }
